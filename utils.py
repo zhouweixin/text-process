66ef621e1f3575928b0e9f9070bfcd9df4a1138f
zhouweixin <1216840597@qq.com>
2019-02-19 16:14:37 +0800

    修复bug

--- utils.py
+++ utils.py
@@ -51,7 +51,7 @@ def extract_info_by_filename(in_path='txt',
             # 复制文件
             try:
                 open(os.path.join(out_path, str(row_num) + '.txt'), 'w', encoding='gbk').write(
-                    open(os.path.join(in_path, file), 'r', encoding='gbk').read().replace(' ', '').replace('\t',
+                    open(os.path.join(in_path, file), 'r', encoding='gbk', errors='ignore').read().replace(' ', '').replace('\t',
                                                                                                            '').replace(
                         '\n', '').replace('\r', ''))
             except:
@@ -60,11 +60,14 @@ def extract_info_by_filename(in_path='txt',
     # 保存错误文件
     if err_files:
         with open('err_files.txt', 'w') as f:
-            f.writelines(err_files)
+            f.write('\n'.join(err_files))
+            # f.writelines(err_files)
 
     datas = pd.DataFrame(datas)
     datas.to_excel(out_file, encoding='gbk', index=False, columns=['序号', '证券代码', '日期', '公告序号', '公司简称', '标题', '移动'])
     print('统计完成, 保存为：' + out_file)
+    if len(err_files) > 0:
+        print('失败文件数：' + str(len(err_files)) + ", 请查看err_files.txt文件")
 
 
 def extract_info_by_rule(in_path='target_txt', out_file='fileinfo.xlsx', rule_name='测试', rule_start='关于',
@@ -184,6 +187,7 @@ def extract_info_by_rule1(in_path='target_txt',
                         name2value[name] = s.group(i + 1)
                     context = s.group()
                     break
+
             if find:
                 # 序号
                 row_num = file.replace('.txt', '')
@@ -192,6 +196,10 @@ def extract_info_by_rule1(in_path='target_txt',
                 row_num2name2value[row_num]['context'] = context
                 # 字段
                 row_num2name2value[row_num]['values'] = name2value
+
+                search = re.search('.{0,50}' + context + '.{0,50}', content)
+                if search:
+                    row_num2name2value[row_num]['context'] = search.group(0)
 
     datas = pd.read_excel(out_file, dtype=str)
 
@@ -284,3 +292,109 @@ def rev(in_path='txt',
             os.remove(os.path.join(in_path, file))
 
     print('删除完成')
+
+
+def move_file_by_title(in_path='D:/助研/成程老师/20190218/政企合作移动文件功能测试文件/新建文件夹',
+                       in_file='D:/助研/成程老师/20190218/政企合作移动文件功能测试文件/一带一路测试文件.xlsx',
+                       out_path='D:/助研/成程老师/20190218/政企合作移动文件功能测试文件/目标文件夹',
+                       fun=6):
+    if not fun == 6:
+        return
+
+    print("【移动函数-根据标题移动】")
+
+    if not os.path.exists(in_path):
+        print('文件夹不存在：' + in_path)
+        return
+
+    if not os.path.exists(in_file):
+        print('文件不存在：' + in_file)
+        return
+
+    # 创建目标路径
+    os.mkdir(out_path) if not os.path.exists(out_path) else 1
+
+    datas = pd.read_excel(in_file, dtype=str)
+    colname = '标题'
+    if colname not in datas.keys():
+        print('标题文件不包含字段：' + colname)
+        return
+
+    titles = []
+    for i, row in datas.iterrows():
+        title = row[colname]
+        if title == 'nan':
+            continue
+
+        titles.append(title)
+
+    files = os.listdir(in_path)
+    for file in files:
+        for title in titles:
+            if title in file:
+                print('move: ' + file)
+                shutil.move(os.path.join(in_path, file), os.path.join(out_path, file))
+                break
+
+    print('移动完成')
+
+
+def clear(in_path='txt',
+          fun=7):
+    if not fun == 7:
+        return
+
+    print("【去重函数-保留时间最小】")
+
+    if not os.path.exists(in_path):
+        print('文件夹不存在：' + in_path)
+        return
+
+    fileinfos = []
+    files = os.listdir(in_path)
+    for file in files:
+        match = re.match(r'(\d{6})-(.*?)\(.*(\d{4}-\d{2}-\d{2})\)\.(txt|pdf)', file)
+        if match:
+            code = match.group(1)
+            title = match.group(2)
+            date = match.group(3)
+
+            fileinfos.append({
+                'file': file,
+                'code': code,
+                'title': title,
+                'date': date})
+
+    code2title2date2file = {}
+    for fileinfo in fileinfos:
+        file = fileinfo['file']
+        code = fileinfo['code']
+        title = fileinfo['title']
+        date = fileinfo['date']
+
+        if code in code2title2date2file:
+            title2date2file = code2title2date2file[code]
+            if title in title2date2file:
+                date2file = title2date2file[title]
+                if date in date2file:
+                    date2file[date].append(file)
+                elif len(date2file.keys()) > 0:
+                    d = list(date2file.keys())[0]
+                    if d > date:
+                        title2date2file[title] = {date: [file]}
+            else:
+                code2title2date2file[code][title] = {date: [file]}
+        else:
+            code2title2date2file[code] = {title: {date: [file]}}
+
+    savefiles = []
+    for title2date2file in code2title2date2file.values():
+        for date2file in title2date2file.values():
+            for file in date2file.values():
+                savefiles.extend(file)
+
+    for file in files:
+        if file not in savefiles:
+            print('delete：' + file)
+            os.remove(os.path.join(in_path, file))
+    print('去重完成')
